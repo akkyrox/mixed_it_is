7b3a3eea63c5fc1f8d43239dd96bbcf9484cafdb
akshay <akshay@kreatio.com>
2014-12-15 14:54:18 +0530

    error checked

--- app/components/06_wpl_admin.rb
+++ app/components/06_wpl_admin.rb
@@ -1,13 +1,13 @@
-SrComponent.define_component("ClassifierList",
-                             :base => 'base',
-                             :decoration=> "/decoration/decoration_classifiers1",
-                             :data_string=> %q{@campaign.classifier_list(params[:classifier_id])},
-                             :partial => 'wpl_admin/wpl_classifier/classifier',
-                             :button => 'wpl_admin/wpl_classifier/add_button',
-                             :xhtml_id => 'existing_fields',
-                             :title => 'Classifier List',
-                             :header => ['Field', 'Value', 'Delete'],
-                             :empty_message => 'No Data Found',
-                             :pagination => false,
-                             :data_as => :collection
-)
+#SrComponent.define_component("ClassifierList",
+ #                            :base => 'base',
+  #                           :decoration=> "/decoration/decoration_classifiers1",
+   #                          :data_string=> %q{@campaign.classifier_list(params[:classifier_id])},
+    #                         :partial => 'wpl_admin/wpl_classifier/classifier',
+     #                        :button => 'wpl_admin/wpl_classifier/add_button',
+      #                       :xhtml_id => 'existing_fields',
+       #                      :title => 'Classifier List',
+        #                     :header => ['Field', 'Value', 'Delete'],
+         #                    :empty_message => 'No Data Found',
+          #                   :pagination => false,
+           #                  :data_as => :collection
+#)
